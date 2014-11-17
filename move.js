8f0a55ff536e8d0272cfff9857796981b29a3626
Heark <hearkofsorcerey@gmail.com>
2014-11-17 10:54:17 -0500

    Update move.js

--- move.js
+++ move.js
@@ -4,3 +4,9 @@ MOVEMENT FOR ROBOT CONTROLLED BY XBOX360 CONTROLLER
 
 
 */
+var pads = Gamepad.getStates();
+for (var i = 0; i < pads.length; ++i) {
+    if (pads[i]) {
+        console.log(i + ": (" + pads[i].leftStickX + ", " + pads[i].leftStickY + ")");
+    }
+}
