2ffed9ea4c1ada8047a0bf967b8170c83e4358dd
Heark <hearkofsorcerey@gmail.com>
2014-11-17 10:48:20 -0500

    Update move.cmd

--- move.cmd
+++ move.cmd
@@ -1,5 +1,9 @@
 :: Movement command for the robot 
 
-moveright = get.controls();
-if moveright = ctrl(r) for ctrl(r) = right{
-instalize 
+if [ $# -ne 2 ]
+then
+    echo "Usage - $0   x    y"
+    echo "        Where x and y are two nos for which I will print sum"
+    exit 1
+fi
+    echo "Sum of $1 and $2 is `expr $1 + $2`"
